3cfb94d3b4fabb757000187c813ad5519f4c935e
plpatel55 <44378181+plpatel55@users.noreply.github.com>
2025-09-15 21:22:19 +0530

    Create playwright.config.ts

---
+++ tests/e2e/playwright.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from '@playwright/test'
+export default defineConfig({
+  testDir: './tests',
+  use: { baseURL: 'http://localhost:3000' },
+  webServer: { command: 'make dev', port: 3000, reuseExistingServer: !process.env.CI },
+})
